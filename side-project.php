10dc27d66b5e25c86cb8513b61d3e08727c0648f
Thunderbird93 <antircristian@gmail.com>
2022-11-30 20:37:15 +0100

    side [add] content side proj

--- side-project.php
+++ side-project.php
@@ -7,6 +7,44 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <span>
+    <img src="./img/saw.jpeg" alt="">
+    <h1>Vogliamo giocare?</h1>
+    <div class="scelta">
+        <button>
+            <a href="./side-project-two.php">Sì, certamente!</a>
+        </button>
+        <button>
+            <a href="./index.php">No, portami indietro.</a>
+        </button>
+    </div>
+    </span>
 </body>
 </html>
+
+<style>
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    img{
+        height: 250px;
+    }
+    body{
+        background-color: white;
+    }
+
+    button{
+        width: 100px;
+        margin: 10px;
+    }
+
+    span{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+</style>
